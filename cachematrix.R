61c4d5eab743cb1a091dccc0eb19c13fd2229617
montielf11 <montielfreddy93@gmail.com>
2024-07-02 21:16:16 -0400

    staging assignment solution

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+inver<-NULL                                                                     ##setting inverse as NULL
+set<-function(y){
+  x<<-y
+inver<<-NULL
+}
+
+get<-function() x                                                               ##function to obtain matrix x
+setInverse<-function(solveMatrix) inver<<-solveMatrix                           
+getInverse<-function() inver                                                    ##Function to obtain inverse
+list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)                    
+
+## Write a short comment describing this function
+        ## Return a matrix that is the inverse of 'x'
 }
 
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...) {                                                ##Obtaining Cache data                           ``
+  inver<-x$getInverse()                                                         ##evaluate if inverse is NULL
+  if(!is.null(inver)) {
+    message("getting cached data!")
+  return(inver)}                                                                ##returns inverse value
+  
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  data<-x$get()
+  inver<-solve(data,...)                                                        ##compute inverse
+  x$setInverse(inver)                                                           ## function returns inverse matrix of x
+  inver
 }
